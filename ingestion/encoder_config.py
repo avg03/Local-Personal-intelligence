d31c2c9c2eb5854e287d68cda29f30ea539bfbb3
avg03 <avgote@gmail.com>
2026-07-08 23:16:23 +0530

    minor changes

--- ingestion/encoder_config.py
+++ ingestion/encoder_config.py
@@ -1,4 +1,9 @@
 # encoder_config.py
-from sentence_transformers import SentenceTransformer
+import os
 
-encoder = SentenceTransformer("all-MiniLM-L6-v2")
+from sentence_transformers import SentenceTransformer
+from dotenv import load_dotenv
+
+load_dotenv()
+
+encoder = SentenceTransformer("all-MiniLM-L6-v2", token=os.getenv("HUGGING_FACE_TOKEN"))
